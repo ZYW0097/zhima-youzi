977de2867304c04c355212a1d62451e687dff42b
ZYW0097 <zywei097@gmail.com>
2024-11-21 12:51:26 +0800

    Update server.js

--- server.js
+++ server.js
@@ -353,6 +353,7 @@ app.get('/line/line_callback', async (req, res) => {
                         phone: reservation.phone  // 使用 redis 中的電話號碼
                     });
                     await userID.save();
+                    window.location.href = 'https://lin.ee/VOKi8ta';
 
                     // 發送綁定成功通知
                     const displayDate = reservation.date.replace(/-/g, '/');
@@ -378,8 +379,6 @@ ${lineName}，您好！
                 }
             }
         }
-
-        res.redirect('/');
 
     } catch (error) {
         console.error('Error:', error.response?.data || error.message);
